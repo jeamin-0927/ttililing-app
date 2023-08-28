0e5c3021cb57cd0aa3086a7334e36e94d2d3d719
최재민 <admin@chicken-moo.com>
2023-08-28 17:02:49 +0900

    chore: eslintrc edit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,69 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
   },
-  extends: [
+  "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors", 
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  "overrides": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json",
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
+  "plugins": ["react", "@typescript-eslint", "import"],
+  "rules": {
     "@typescript-eslint/no-loss-of-precision": "off",
-    indent: ["error", 2],
+    "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
     "react/prop-types": "off",
     "react/display-name": "off",
     "react-native/no-inline-styles": "off",
     "react-hooks/exhaustive-deps": "off",
     "@typescript-eslint/no-unused-vars": "warn",
     "no-unused-vars": "warn",
-    // "import/order": [
-    //   "error",
-    //   {
-    //     groups: [
-    //       "builtin",
-    //       "external",
-    //       "internal",
-    //       "parent",
-    //       "sibling",
-    //       "index",
-    //     ],
-    //     alphabetize: {
-    //       order: "asc",
-    //       caseInsensitive: true,
-    //     },
-    //     "newlines-between": "always",
-    //   },
-    // ],
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "unknown",
+        ],
+        "pathGroups": [],
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true,
+        },
+        "newlines-between": "always",
+      },
+    ],
   },
+  "settings": {
+    "react": {
+      "version": "detect"
+    },
+    "import/ignore": ["react-native"],
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      },
+      "typescript": {}
+    },
+  }
 };
